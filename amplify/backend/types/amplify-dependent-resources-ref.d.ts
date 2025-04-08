44aa6c3a93b37cfbd24ffb3aa53468d252d3bd46
Mohammadreza Montazerijouybari <mohammad.mntj@gmail.com>
2025-04-07 21:21:08 -0400

    ✅ Add aws-exports.js from Amplify push

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "robomeansdashboard9e7258da": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
